5e7c95a982728f0bb63476b6905c8ef290af8fc4
Juan Fajardo <bett0@potosi.bo>
2018-12-06 10:06:44 -0400

    Usuarios

---
+++ database/migrations/tmp/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('username');
+            $table->string('password');
+
+            $table->string('nombre');
+            $table->string('paterno');
+            $table->string('materno');
+            $table->string('ci');
+            $table->string('cargo');
+
+            $table->rememberToken();
+            $table->timestamps();
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
